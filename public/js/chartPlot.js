b2e319a44ffe75a4d5be855108267b91f39a015a
Rahul Goyal <rahul.goyal@studypadinc.com>
2014-09-21 19:26:20 +0530

    minor changes done

--- public/js/chartPlot.js
+++ public/js/chartPlot.js
@@ -7,6 +7,10 @@
       var dataConverted = {};
       var ithData;
 
+      var dropdown1 = document.getElementById("dropdown1");
+      var dropdown2 = document.getElementById("dropdown2");
+      var opt;
+
       for (var i = 1; i < dataParsed.length; i++) {
         ithData = dataParsed[i];
         for (var j = 0; j < ithData.length; j++) {
@@ -15,15 +19,21 @@
             dataConverted[paramName].push(dataParsed[i][j]);
           } else {
             dataConverted[paramName] = [dataParsed[i][j]];
+            opt = document.createElement("option");
+            opt.text = paramName;
+            opt.value = paramName;
+            dropdown1.options.add(opt);
+            dropdown2.options.add(opt);
           }
         }
       }
+      dropdown1.style.display = 'block';
+      dropdown2.style.display = 'block';
+    },
 
-      console.log(dataConverted["Name"]);
-      console.log(dataConverted["Age"]);
-
-
+    graphPlot:function(value1,value2){
       var data = {
+        labels: value1,
         labels: dataConverted["Name"],
         datasets: [
           {
@@ -32,14 +42,13 @@
             strokeColor: "rgba(220,220,220,0.8)",
             highlightFill: "rgba(220,220,220,0.75)",
             highlightStroke: "rgba(220,220,220,1)",
-            data: dataConverted["Age"]
+            data: value2
+            // data: dataConverted["Age"]
           }
         ]
       };
-
       var ctx = $("#myChart").get(0).getContext("2d");
       var options = {};
-
       var myBarChart = new Chart(ctx).Bar(data, options);
     }
   }
